675c647028133022835df78f2fb210f664b66486
Maksim Ivanov <ivanovmaximant@gmail.com>
2020-07-07 11:54:33 +0300

    Create models

--- imageboard_app/admin.py
+++ imageboard_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from imageboard_app.models import Post, Reply
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject',)
+    prepopulated_fields = {'slug': ('subject',)}
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Reply, ReplyAdmin)
--- imageboard_app/models.py
+++ imageboard_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Post(models.Model):
+    image = models.FileField(upload_to='imageboard/posts/')
+    subject = models.TextField(max_length=40)
+    description = models.TextField(max_length=600)
+    date_added = models.DateField(default=timezone.now)
+    slug = models.SlugField(null=False, unique=True)
+
+    class Meta:
+        ordering = ['-date_added',]
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.subject)
+        return super(Post, self).save(*args, **kwargs)
+
+class Reply(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='replies')
+    text = models.TextField(max_length=400)
+
+    class Meta:
+        verbose_name_plural = 'replies'
+
+    def __str__(self):
+        return self.text
